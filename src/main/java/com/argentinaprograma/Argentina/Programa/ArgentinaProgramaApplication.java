052e17d86993d162fb288cb196a8f443da029cf3
MARIA EUGENIA DALLASTA <71835912+eugedallasta@users.noreply.github.com>
2022-05-29 11:38:49 -0300

    Update ArgentinaProgramaApplication.java

--- src/main/java/com/argentinaprograma/Argentina/Programa/ArgentinaProgramaApplication.java
+++ src/main/java/com/argentinaprograma/Argentina/Programa/ArgentinaProgramaApplication.java
@@ -15,21 +15,22 @@ public class ArgentinaProgramaApplication {
 		SpringApplication.run(ArgentinaProgramaApplication.class, args);
 	}
         
-        @Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200" ,"https://portfolio-deploy-b11f2.web.app/portfolio"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+        
+	@Bean
+		public WebMvcConfigurer corsConfigurer() {
+   		 return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("https://portfolio-deploy-b11f2.web.app%22 , "http://localhost:4200")
+                    .allowedMethods(
+                            "GET",
+                            "POST",
+                            "PUT",
+                            "DELETE");
+        }
+    };
+}
 
 
 }
